a884a4c8227a9550030ff937c75b67cf18084777
chrisdavid32 <chrisdavid3271@gmail.com>
2022-12-08 09:33:58 -0800

    add index and admin template

--- class/MoviesController.php
+++ class/MoviesController.php
@@ -8,5 +8,15 @@ class MoviesController{
     {
         $this->crud = new Crud();
     }
+
+    public function addMovies()
+    {
+        $data = [
+            'mv_title' => $_POST['mv_title'],
+            'mv_year_released' => $_POST['mv_year_released']
+        ];
+
+        $this->crud->create($data, 'movies');
+    }
     
 }
